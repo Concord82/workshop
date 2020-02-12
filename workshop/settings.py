091389bf22dc731d2e595b3a973dcab719c24971
lviv <emeriroot@gmail.com>
2020-02-12 23:11:15 +0700

    fixup! fixup! User model modification

--- workshop/settings.py
+++ workshop/settings.py
@@ -80,9 +80,18 @@ WSGI_APPLICATION = 'workshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'workshop_db',
+        'USER': 'workshop',
+        'PASSWORD': 'workshop',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
 }
 
 
